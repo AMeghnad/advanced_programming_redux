920c0ac08b7bf80ed81da570788c4a9dae335e34
AMeghnad <anshumanmeghnad@gmail.com>
2018-11-05 13:34:14 +1100

    Initial commit

---
+++ Assets/Scripts/GameSave.cs
@@ -0,0 +1,87 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// --- XML Saving namespaces---//
+using System.Xml;
+using System.Xml.Serialization;
+using System.IO;
+
+[XmlRoot("Game Data")]
+public class GameData
+{
+    //---All game data able to be saved to a file---//
+    public Vector3 playerPos;
+    public int score;
+    public Quaternion playerRot;
+    public void Save(string filePath)
+    {
+        // Create a tool for converting class into XML
+        XmlSerializer serializer = new XmlSerializer(typeof(GameData));
+        // Generate a file stream and open file stream to path
+        using (var stream = new FileStream(filePath, FileMode.Create))
+        {
+            // Save file to path
+            serializer.Serialize(stream, this);
+        }
+    }
+
+    public void Load(string filePath)
+    {
+        if (File.Exists(filePath))
+        {
+            // Create a tool for converting XML into class
+            XmlSerializer serializer = new XmlSerializer(typeof(GameData));
+            // Generate a file stream with the mode 'Open' to read file
+            using (var stream = new FileStream(filePath, FileMode.Open))
+            {
+                GameData data = serializer.Deserialize(stream) as GameData;
+                //---Load the game data values here---//
+                playerPos = data.playerPos;
+                score = data.score;
+                playerRot = data.playerRot;
+            }
+        }
+    }
+}
+
+public class GameSave : MonoBehaviour
+{
+    #region Singleton
+    public static GameSave Instance = null;
+    private void Awake()
+    {
+        Instance = this;
+        Load();
+    }
+    public static GameData GetData()
+    {
+        return Instance.data;
+    }
+    #endregion
+
+    public GameData data = new GameData();
+    public string fileName = "GameSave";
+
+    public void Save()
+    {
+        string fullPath = Application.dataPath + "/Data/" + fileName + ".xml";
+        data.Save(fullPath);
+        print("Saved to path: " + fullPath);
+        // C:/Users/anshuman.meghnad/Documents/advanced_programming/AssetsGameSave.xml
+    }
+
+    public void Load()
+    {
+        string fullPath = Application.dataPath + "/Data/" + fileName + ".xml";
+        data.Load(fullPath);
+        print("Loaded from path: " + fullPath);
+        // C:/Users/anshuman.meghnad/Documents/advanced_programming/AssetsGameSave.xml
+    }
+
+    public static bool Exists()
+    {
+        string fullPath = Application.dataPath + "/Data/" + Instance.fileName + ".xml";
+        return File.Exists(fullPath);
+    }
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public Transform cam;
+
+    public GameObject weapon;
+
+    public float speed = 6.0F;
+    public float jumpSpeed = 8.0F;
+    public float gravity = 20.0F;
+    private Vector3 moveDirection = Vector3.zero;
+    private int score;
+
+    public CharacterController controller;
+
+    private void Start()
+    {
+        GameData data = GameSave.GetData();
+        if (GameSave.Exists())
+        {
+            // Get the data
+            data = GameSave.GetData();
+            transform.position = data.playerPos;
+            score = data.score;
+            transform.rotation = data.playerRot;
+        }
+
+    }
+
+    private void OnDestroy()
+    {
+        // Save position when player is destroyed
+        GameData data = GameSave.GetData();
+        data.playerPos = transform.position;
+        GameSave.Instance.Save();
+    }
+
+    public void Move(float inputH, float inputV, bool isJumping)
+    {
+        if (controller.isGrounded)
+        {
+            // Rotate the player in the direction of camera
+            Vector3 euler = cam.transform.eulerAngles;
+            transform.rotation = Quaternion.AngleAxis(euler.y, Vector3.up);
+
+            moveDirection = new Vector3(inputH, 0, inputV);
+            moveDirection = transform.TransformDirection(moveDirection);
+            moveDirection *= speed;
+
+            if (isJumping)
+            {
+                moveDirection.y = jumpSpeed;
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        moveDirection.y -= gravity * Time.deltaTime;
+        controller.Move(moveDirection * Time.deltaTime);
+
+        if (Input.GetKeyDown(KeyCode.Return))
+        {
+            weapon.SetActive(true);
+        }
+    }
+}
